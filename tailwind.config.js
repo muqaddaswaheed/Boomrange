358bb81460115f3dc43332882ae7ae85bd39938c
muqaddas <muqaddaswaheed0@gmail.com>
2025-01-13 09:26:38 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      karla: ["Karla", "sans-serif"], // Add your font here
+    },
+  },
+  plugins: [],
+}
+
